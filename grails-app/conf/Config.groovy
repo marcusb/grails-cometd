3da878dc668f998c70d4821d62522bc96b9b8268
mingfai <mingfai@832c1c66-4827-0410-8465-ccb17913cd09>
2009-02-20 04:24:20 +0000

    updated
    git-svn-id: http://svn.codehaus.org/grails-plugins/grails-cometd/trunk@47838 832c1c66-4827-0410-8465-ccb17913cd09

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -11,67 +11,55 @@
 // }
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
 grails.mime.use.accept.header = false
-grails.mime.types = [html: ['text/html', 'application/xhtml+xml'],
-        xml: ['text/xml', 'application/xml'],
-        text: 'text-plain',
-        js: 'text/javascript',
-        rss: 'application/rss+xml',
-        atom: 'application/atom+xml',
-        css: 'text/css',
-        csv: 'text/csv',
-        all: '*/*',
-        json: ['application/json', 'text/json'],
-        form: 'application/x-www-form-urlencoded',
-        multipartForm: 'multipart/form-data'
-]
+grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
+                      xml: ['text/xml', 'application/xml'],
+                      text: 'text/plain',
+                      js: 'text/javascript',
+                      rss: 'application/rss+xml',
+                      atom: 'application/atom+xml',
+                      css: 'text/css',
+                      csv: 'text/csv',
+                      all: '*/*',
+                      json: ['application/json','text/json'],
+                      form: 'application/x-www-form-urlencoded',
+                      multipartForm: 'multipart/form-data'
+                    ]
 // The default codec used to encode data with ${}
-grails.views.default.codec = "none" // none, html, base64
-grails.views.gsp.encoding = "UTF-8"
-grails.converters.encoding = "UTF-8"
+grails.views.default.codec="none" // none, html, base64
+grails.views.gsp.encoding="UTF-8"
+grails.converters.encoding="UTF-8"
 
 // enabled native2ascii conversion of i18n properties files
 grails.enable.native2ascii = true
 
 // set per-environment serverURL stem for creating absolute links
 environments {
-  production {
-    grails.serverURL = "http://grails.org/Cometd+Plugin"
-  }
+    production {
+        grails.serverURL = "http://www.changeme.com"
+    }
 }
 
 // log4j configuration
 log4j = {
-  appenders {
-    console name: 'consoleAppender', layout: pattern(conversionPattern: "%d{ABSOLUTE} %-5p [%c{1}][%X{tid}] %m%n")
-  }
-  debug consoleAppender: 'org.grails.plugins.cometd'
+    // Example of changing the log pattern for the default console
+    // appender:
+    //
+    //appenders {
+    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+    //}
 
-  error 'org.codehaus.groovy.grails.web.servlet',  //  controllers
-          'org.codehaus.groovy.grails.web.pages', //  GSP
-          'org.codehaus.groovy.grails.web.sitemesh', //  layouts
-          'org.codehaus.groovy.grails."web.mapping.filter', // URL mapping
-          'org.codehaus.groovy.grails."web.mapping', // URL mapping
-          'org.codehaus.groovy.grails.commons', // core / classloading
-          'org.codehaus.groovy.grails.plugins', // plugins
-          'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
-          'org.springframework',
-          'org.hibernate'
+    error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
+	       'org.codehaus.groovy.grails.web.pages', //  GSP
+	       'org.codehaus.groovy.grails.web.sitemesh', //  layouts
+	       'org.codehaus.groovy.grails."web.mapping.filter', // URL mapping
+	       'org.codehaus.groovy.grails."web.mapping', // URL mapping
+	       'org.codehaus.groovy.grails.commons', // core / classloading
+	       'org.codehaus.groovy.grails.plugins', // plugins
+	       'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
+	       'org.springframework',
+	       'org.hibernate'
 
-  warn 'org.mortbay.log'
-}
-
-plugins {
-  cometd {
-    'servlet-mapping' {
-        namespace = 'cometd'
-    }
-    'noCacheFilter' {
-      disable = false; //the filter add HTTP Header for disabling HTTP cache
-    }
-    'cometdService' { // Cometd provides extra functions including logging and client tracking
-      disable = true; // disable
-    }
-  }
+    warn   'org.mortbay.log'
 }
 
 
